e18d4a1304af10041dd23f3e45d0577114d32e8c
catherinism <catcreates13@gmail.com>
2019-11-25 21:20:23 -0500

    generate mood resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :moods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :moods
     end
   end
 end
