c196540f9af624aa7cc828a8975decc111e73b6e
catherinism <catcreates13@gmail.com>
2019-11-25 21:25:07 -0500

    generate quote resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :quotes
   resources :moods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
