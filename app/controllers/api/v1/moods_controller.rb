aa673ff455fd9bc6a7599258eb8140e8c28f4584
catherinism <catcreates13@gmail.com>
2019-12-07 17:10:03 -0500

    define actions and render json

--- app/controllers/api/v1/moods_controller.rb
+++ app/controllers/api/v1/moods_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::MoodsController < ApplicationController
     def index
         @moods = Mood.all
-    
         render json: @moods
       end
+
+    def show
+      @mood = Mood.find_by(id: params[:id])
+      render json: @mood
+    end
+
+    def create
+      mood = Mood.new(mood_params)
+      render json: mood
+    end
+
+    private
+    def mood_params
+      params.require(:mood).permit(:feeling)
+    end
+
 end
